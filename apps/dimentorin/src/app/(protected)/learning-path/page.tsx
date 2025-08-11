380c519007341f7408f818465140fe41f59a99de
hafidnrzs <hafidnurazis@gmail.com>
2025-08-11 23:40:48 +0700

    roadmap discovery & learning path page

--- apps/dimentorin/src/app/(protected)/learning-path/page.tsx
+++ apps/dimentorin/src/app/(protected)/learning-path/page.tsx
@@ -1,16 +1,99 @@
+import { NavbarDashboardUser } from '@imphnen-frontend-service/ui/organisms';
 import { FC, ReactElement } from 'react';
 
 export const Components: FC = (): ReactElement => {
   return (
-    <div className="w-full px-[48px] py-[40px] flex flex-col gap-8">
-      <div className="flex flex-col justify-between gap-[40px] p-8 bg-white rounded-md">
-        <section className="w-full flex flex-col gap-[40px]">
-          <h2 className="text-p2 font-medium text-primary-500">
-            Learning Path
-          </h2>
-        </section>
+    <main className="w-full px-[48px] py-[40px] flex flex-col gap-8">
+      <div className="flex flex-col justify-between gap-6">
+        <NavbarDashboardUser />
       </div>
-    </div>
+
+      <nav className="bg-primary-100 inline-flex items-center rounded-sm p-1 gap-1 self-start">
+        <div className="rounded-sm shadow-sm font-semibold text-p3 text-primary-500 bg-white py-1 px-2.5 select-none cursor-pointer">
+          Roadmap
+        </div>
+        <div className="rounded-sm font-semibold text-p3  py-1 px-2.5 select-none cursor-pointer text-primary-300">
+          Article
+        </div>
+      </nav>
+
+      <div className="bg-white p-10 rounded-lg shadow-sm">
+        <p className="text-neutral-400 text-label1 font-medium">Roadmap Kamu</p>
+        <h2 className="font-semibold text-p1 text-primary-500 mb-7">
+          Front-end Basic
+        </h2>
+
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between bg-neutral-50 rounded-2xl px-5 py-3">
+            <p className="text-p2 font-semibold text-primary-500">
+              Day 1 - Materi A
+            </p>
+            <p className="text-primary-500 font-semibold text-p3">
+              1/3 Diselesaikan
+            </p>
+          </div>
+          <div className="pl-6 flex flex-col gap-2">
+            <div className="flex justify-between items-center bg-neutral-50 rounded-2xl px-5 py-3">
+              <p className="text-p2 font-semibold text-neutral-600">
+                1. Submateri 1
+              </p>
+              <div className="bg-success-200 text-success-500 font-semibold text-label1 rounded-sm px-3 py-1.5">
+                Done
+              </div>
+            </div>
+            <div className="flex justify-between items-center rounded-2xl px-5 py-3">
+              <p className="text-p2 font-semibold text-neutral-600">
+                2. Submateri 2
+              </p>
+              <div className="bg-primary-100 text-primary-500 font-semibold text-label1 rounded-sm px-3 py-1.5">
+                To do
+              </div>
+            </div>
+            <div className="flex justify-between items-center rounded-2xl px-5 py-3">
+              <p className="text-p2 font-semibold text-neutral-600">
+                3. Tugas: Membuat Artikel
+              </p>
+              <div className="bg-primary-100 text-primary-500 font-semibold text-label1 rounded-sm px-3 py-1.5">
+                To do
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between  rounded-2xl px-5 py-3">
+            <p className="text-p2 font-semibold text-primary-500">
+              Day 2 - Materi B
+            </p>
+            <p className="text-danger-400 font-semibold text-p3">
+              Selesaikan materi sebelumnya
+            </p>
+          </div>
+          <div className="flex justify-between  rounded-2xl px-5 py-3">
+            <p className="text-p2 font-semibold text-primary-500">
+              Day 3 - Materi C
+            </p>
+            <p className="text-danger-400 font-semibold text-p3">
+              Selesaikan materi sebelumnya
+            </p>
+          </div>
+          <div className="flex justify-between  rounded-2xl px-5 py-3">
+            <p className="text-p2 font-semibold text-primary-500">
+              Day 4 - Materi D
+            </p>
+            <p className="text-danger-400 font-semibold text-p3">
+              Selesaikan materi sebelumnya
+            </p>
+          </div>
+          <div className="flex justify-between  rounded-2xl px-5 py-3">
+            <p className="text-p2 font-semibold text-primary-500">
+              Day 5 - Materi E
+            </p>
+            <p className="text-danger-400 font-semibold text-p3">
+              Selesaikan materi sebelumnya
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
--- apps/dimentorin/src/app/(protected)/roadmap-discovery/page.tsx
+++ apps/dimentorin/src/app/(protected)/roadmap-discovery/page.tsx
@@ -1,16 +1,50 @@
+import { Button, Input } from '@imphnen-frontend-service/ui/atoms';
+import { NavbarDashboardUser } from '@imphnen-frontend-service/ui/organisms';
 import { FC, ReactElement } from 'react';
 
 export const Components: FC = (): ReactElement => {
   return (
-    <div className="w-full px-[48px] py-[40px] flex flex-col gap-8">
-      <div className="flex flex-col justify-between gap-[40px] p-8 bg-white rounded-md">
-        <section className="w-full flex flex-col gap-[40px]">
-          <h2 className="text-p2 font-medium text-primary-500">
-            Roadmap Dicovery
-          </h2>
+    <main className="w-full px-[48px] py-[40px] flex flex-col gap-4">
+      <div className="flex flex-col justify-between gap-6">
+        <NavbarDashboardUser />
+      </div>
+
+      <div className="min-h-[calc(100dvh-150px)] flex items-center justify-center">
+        <section className="flex flex-col items-center gap-10">
+          <div className="text-center">
+            <p className="text-p1 text-neutral-400 leading-tight">
+              Welcome to{' '}
+              <span className="text-primary-500 font-semibold">
+                Roadmap Discovery
+              </span>
+            </p>
+            <p className="text-neutral-600 font-bold text-h1">
+              Start Your <span className="text-primary-500">Journey</span>
+            </p>
+          </div>
+
+          <div className="flex">
+            <img src="/image/mascot1.webp" alt="" width={146} />
+            <div className="shadow-md p-4 text-p2 text-neutral-500 bg-white self-start w-[380px]">
+              Lagi pengen belajar apa? Ketik aja di sini, biar AI bantuin bikin
+              roadmap-nya.
+            </div>
+          </div>
+
+          <div className="flex gap-4">
+            <Input placeholder="Mau belajar roadmap apa?" size="lg" />
+            <select className="px-[12px] py-[8px] text-neutral-800 bg-white placeholder:text-neutral-300 border border-neutral-200 hover:border-blue-300 focus:outline-1 focus:outline-blue-500 rounded-md font-bai-jamjuree            w-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-p3">
+              <option value="">Tingkat Belajar</option>
+              <option value="pemula">Pemula</option>
+              <option value="menengah">Menengah</option>
+              <option value="mahir">Mahir</option>
+            </select>
+          </div>
+
+          <Button size="lg">Generate</Button>
         </section>
       </div>
-    </div>
+    </main>
   );
 };
 
